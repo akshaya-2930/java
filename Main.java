a1f13cb6e33b551a0a186796c1a608abb8ee0bd2
akshaya-2930 <akkiakshaya142@gmail.com>
2025-10-09 10:04:12 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,20 @@
+// class Student {
+//     // data member (also instance variable)
+//     int id;
+//     // data member (also instance variable)
+//     String n;}
+//     public class Main{
+//         public static void main(String args[]) {
+//             // creating an object of
+//             // Student
+//             Student s1 = new Student();
+//             Student s2 = new Student();
+//             Student s3 = new Student();
+//             Student s4 = new Student();
+//             System.out.println(s1.id);
+//             System.out.println(s1.n);
+//         }
+//     }
+
+
+
